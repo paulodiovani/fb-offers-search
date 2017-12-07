29b996d373500f3ff0b459b86ab9dcda53e315ae
Paulo Diovani <paulo@diovani.com>
2017-12-07 13:31:27 -0200

    Search with elasticlunr

--- src/search/Search.js
+++ src/search/Search.js
@@ -1,30 +1,57 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import elasticlunr from 'elasticlunr'
 import './Search.css'
 
 const groupIds = [
-  // '1002772103090233',
+  '1002772103090233',
   '502867033154810'
 ]
 
 class Search extends Component {
+  constructor (props) {
+    super(props)
+
+    this.documents = []
+    this.index = elasticlunr(function () {
+      this.setRef('id')
+      this.addField('message')
+    })
+  }
+
   shouldComponentUpdate () {
     return false
   }
 
-  componentDidMount () {
-    let results = []
+  addDoc (doc) {
+    this.documents.push(doc)
+    this.index.addDoc(doc)
+  }
 
+  populate (documents) {
+    this.props.populate(documents)
+  }
+
+  componentDidMount () {
     groupIds.forEach((gid) => {
       window.FB.api(
         `/${gid}/feed?fields=message,picture,permalink_url,created_time`,
         (response) => {
-          results = results.concat(response.data)
-          results.sort((a, b) => a.created_time < b.created_time ? -1 : 1)
-          this.props.populate(results)
+          response.data.forEach((doc) => this.addDoc(doc))
         }
       )
     })
+  }
+
+  search (el) {
+    const results = this.index.search(el.target.value, {
+      bool: 'AND',
+      expand: true
+    })
+    const documents = results.map((item) => {
+      return this.documents.find((doc) => doc.id === item.ref)
+    })
+    this.populate(documents)
   }
 
   render () {
@@ -33,7 +60,7 @@ class Search extends Component {
         <fieldset>
           <label htmlFor="search"></label>
           <div className="form-group">
-            <input type="text" id="search"/>
+            <input type="text" id="search" onChange={this.search.bind(this)}/>
             <input className="button-primary" type="submit" value="Search"/>
           </div>
         </fieldset>
